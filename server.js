24a00bafb298d5dfd0c120ece4291d375114d56d
Pete Evans <pete@widgetdude.com>
2022-09-13 09:41:52 -0400

    Add Cors

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const server = require("http").createServer();
-const io = require("socket.io")(server);
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
+
+const PORT = 3000;
 
 io.on("connection", (socket) => {
   console.log("New client connected", socket.id);
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
